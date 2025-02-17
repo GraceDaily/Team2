b477538c707a25f73424c98af6bdda36a5198d48
GraceDaily <123136176+GraceDaily@users.noreply.github.com>
2025-02-17 21:46:48 +0000

    indent

--- src/Pages/Library.js
+++ src/Pages/Library.js
@@ -5,7 +5,8 @@ const Library = () => {
         <>
           <div className="container">
             <div className="row py-5 my-5">
-              <div className="col-12 mt-2 mb-4 fs-1 fw-bold text-decoration-underline head d-flex justify-content-center align-items-center">
+              <div className="col-12 mt-2 mb-4 fs-1 fw-bold text-decoration-underline head 
+              d-flex justify-content-center align-items-center">
                 Library
               </div>
             </div>
--- src/Pages/MovieSwpie.js
+++ src/Pages/MovieSwpie.js
@@ -5,7 +5,8 @@ const MovieSwpie = () => {
         <>
           <div className="container">
             <div className="row py-5 my-5">
-              <div className="col-12 mt-2 mb-4 fs-1 fw-bold text-decoration-underline head d-flex justify-content-center align-items-center">
+              <div className="col-12 mt-2 mb-4 fs-1 fw-bold text-decoration-underline head 
+              d-flex justify-content-center align-items-center">
                 MovieSwipe
               </div>
             </div>
